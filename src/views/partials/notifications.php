9eca5f137cf662e2b06a2aba396cbac946e7b9a9
b1nj <postmaster@b1nj.fr>
2014-09-23 21:18:36 -0400

    change html structure message

--- src/views/partials/notifications.php
+++ src/views/partials/notifications.php
@@ -1,9 +1,9 @@
-<div class="messages <?= $type ?>">
-    <button type="button" class="closeMessage" data-dismiss="alert">&times;</button>
-    <p><?= $label ?> :</p>
-    <ul>
+<div class="messages messages-<?= $type ?>">
+    <button type="button" class="messages-close" onclick="this.parentNode.classList.add('is-close');" data-dismiss="alert">&times;</button>
+    <p class="messages-label"><?= $label ?> :</p>
+    <ul class="messages-list">
         <?php foreach ($messages as $message) : ?>
-        <li><?= $message ?></li>
+        <li class="messages-items"><?= $message ?></li>
         <?php endforeach ?>
     </ul>
 </div>
