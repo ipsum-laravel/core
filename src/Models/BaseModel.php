e37e098cd7b73b4eb640f77f59cf82286db9396c
B1nj <postmaster@b1nj.fr>
2016-10-03 12:40:11 -0400

    Update BaseModel

--- src/Models/BaseModel.php
+++ src/Models/BaseModel.php
@@ -5,8 +5,26 @@ use Eloquent;
 use Validator;
 
 class BaseModel extends Eloquent {
+    
+    protected $nullable = [];
+    
+    /**
+     * Set empty nullable fields to null
+     * @param object $model
+     */
+    protected static function setNullables($model)
+    {
+        foreach($model->nullable as $field)
+        {
+            if($model->{$field} === '')
+            {
+                $model->{$field} = null;
+            }
+        }
+    }    
 
-    public static function validate($data, $message = array()) {
-        return Validator::make($data , static::$rules, $message);
+    public static function validate($data, $rules = null, $message = array()) {
+        $rules = $rules === null ? static::getRules() : $rules;
+        return Validator::make($data , $rules, $message);
     }
 }
